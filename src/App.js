21a68c7904cd95e844b875bc2c7ff9655b87d371
DUKEadiaN <71949237+DUKEadiaN@users.noreply.github.com>
2020-12-07 00:48:48 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component} from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      {/*<logo />
+      <ImageLinkForm />
+      <FaceRecognition />*/}
+    </div>
+  );
+}
+
+export default App;
